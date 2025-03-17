7d4ddb456dda64ec0f90d9d0989cd9df16dbeda5
zbares <baresova.zuzana@gmail.com>
2025-03-17 20:09:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-// gets a new object (the architecture allows us to not have to use the 'new' keyword here)
-var g = G$('Jane', 'Doe');
-g.greet().setLang('es').greet(true).log();
+//using the object with console:
+// create a new object 
+var g = G$("Z", "B");
+g.greet().setLang("es").greet(true).log();
 
+
+//using the object with HTML:
 // let's use our object on the click of the login button
-$('#login').click(function () {
+// $('#login'): This part uses jQuery's selector to find the HTML element with the ID "login". The # symbol indicates that it's selecting an element by its ID. 
+// .click(function () { ... }): This is a jQuery method that attaches a function to be executed when the selected element is clicked. 
+// function () { ... }: This is an anonymous function (a function without a name) that contains the code to be executed when the "login" element is clicked.
 
-    // create a new 'Greetr' object (let's pretend we know the name from the login)
-    var loginGrtr = G$('John', 'Doe');
+// we are passing function object that is created on the fly to the click event
+$("#login").click(function () {
+
+    // create a new 'Greetr' object the name from the login is "known" from login, as if returned by server
+    var loginGrtr = G$("Zuz", "Bar");
 
     // hide the login on the screen
-    $('#logindiv').hide();
+    $("#logindiv").hide();
 
+    // HTML Greeting creates the JQuery object, only selector needs to be added
     loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
 
 });
